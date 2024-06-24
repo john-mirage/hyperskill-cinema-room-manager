7b7b18fc7bb3141a74cb8381b5468d0101da1b46
John Mirage <28307090+john-mirage@users.noreply.github.com>
2024-06-24 15:29:08 +0200

    Update Cinema.java

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -3,35 +3,42 @@ package cinema;
 public class Cinema {
 
     public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        int rows = ui.askForRows();
-        int seats = ui.askForSeats();
-        ScreenRoom sr = new ScreenRoom(rows, seats);
+        int rows = UserInterface.askForNumber("Enter the number of rows:", 0, 10);
+        int seats = UserInterface.askForNumber("Enter the number of seats in each row:", 0, 10);
+        ScreenRoom screenRoom = new ScreenRoom(rows, seats);
+        mainLoop:
         while (true) {
-            int choice = ui.askForChoice();
-            if (choice == 0) {
-                break;
-            } else {
-                if (choice == 1) {
-                    System.out.println(sr);
+            int choice = UserInterface.askForNumber("1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit", 0, 3);
+            switch (choice) {
+                case 0: {
+                    break mainLoop;
                 }
-                if (choice == 2) {
+                case 1: {
+                    System.out.println(screenRoom);
+                    break;
+                }
+                case 2: {
                     while (true) {
-                        int row = ui.askForRow(sr.getRows());
-                        int seat = ui.askForSeat(sr.getSeats());
-                        if (sr.takeSeat(row, seat)) {
-                            System.out.println("Ticket price: $" + sr.getTicketPrice(row));
+                        int row = UserInterface.askForNumber("Enter a row number:", 0, screenRoom.getRows());
+                        int seat = UserInterface.askForNumber("Enter a seat number in that row:", 0, screenRoom.getSeats());
+                        if (screenRoom.takeSeat(row, seat)) {
+                            System.out.println("Ticket price: $" + screenRoom.getTicketPrice(row));
                             break;
                         } else {
                             System.out.println("That ticket has already been purchased!");
                         }
                     }
+                    break;
                 }
-                if (choice == 3) {
-                    System.out.println("Number of purchased tickets: " + sr.getPersons());
-                    System.out.println("Percentage: " + String.format("%.2f", sr.getPercentageOfPersons()) + "%");
-                    System.out.println("Current income: $" + sr.getCurrentIncome());
-                    System.out.println("Total income: $" + sr.getTotalIncome());
+                case 3: {
+                    System.out.println("Number of purchased tickets: " + screenRoom.getPersons());
+                    System.out.println("Percentage: " + String.format("%.2f", screenRoom.getPercentageOfPersons()) + "%");
+                    System.out.println("Current income: $" + screenRoom.getCurrentIncome());
+                    System.out.println("Total income: $" + screenRoom.getTotalIncome());
+                    break;
+                }
+                default: {
+                    System.out.println("Invalid choice");
                 }
             }
         }
